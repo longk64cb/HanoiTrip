c2e6a1da2a8231e778bafbf7574d1eb4c250e4e8
Tran Dinh Long <longa5k50@gmail.com>
2021-10-22 13:30:24 +0700

    cấu trúc lại file

--- Streamline/index.php
+++ Streamline/index.php
@@ -1,13 +1,16 @@
 <?php
-	require_once('../lib/functions.php');
+	// require_once('../lib/functions.php');
 
-	$d = initializeApp('streamline');	
-	$ni = sizeof($d['items']);
-	$iu = array($ni);	
+	// $d = initializeApp('streamline');	
+	// $ni = sizeof($d['items']);
+	// $iu = array($ni);	
 
-	for($i=0;$i<$ni;$i++){
-		$itemid = $d['items'][$i];
-		$idata 	= fetchItemData($itemid);
-	}
-  include("../index.html");
+	// for($i=0;$i<$ni;$i++){
+	// 	$itemid = $d['items'][$i];
+	// 	$idata 	= fetchItemData($itemid);
+	// }
+?>
+
+<?php
+  include('../html/index.html');
 ?>
--- Streamline/map.php
+++ Streamline/map.php
@@ -1,3 +1,3 @@
 <?php
-    include("../map.html");
+    include('../html/map.html');
 ?>
