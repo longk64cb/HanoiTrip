ba5305e728267374b5bd1a853b7532165dd55be4
Tran Dinh Long <longa5k50@gmail.com>
2021-10-24 09:37:02 +0700

    update cấu trúc file

---
+++ introPage/index.php
@@ -0,0 +1,16 @@
+<?php
+	// require_once('../lib/functions.php');
+
+	// $d = initializeApp('streamline');	
+	// $ni = sizeof($d['items']);
+	// $iu = array($ni);	
+
+	// for($i=0;$i<$ni;$i++){
+	// 	$itemid = $d['items'][$i];
+	// 	$idata 	= fetchItemData($itemid);
+	// }
+?>
+
+<?php
+  include('../html/index.html');
+?>
