c0714c6a4df9d2e13200e37b5ce7124706e2018c
Tran Dinh Long <longa5k50@gmail.com>
2021-11-11 07:48:47 +0700

    update admin

--- admin/script.js
+++ admin/script.js
@@ -9,17 +9,19 @@ add_more_fields.onclick = function(){
 	// newField.setAttribute('class','survey_options');
 	newField.setAttribute('class','typeOfItem');
 	newField.setAttribute('siz',50);
-	newField.setAttribute('placeholder','Loại item');
+	newField.setAttribute('placeholder','Title');
 	survey_options.appendChild(newField);
 
 	var newField2 = document.createElement('input');
 	newField2.setAttribute('type','text');
 	newField2.setAttribute('name','survey_options[]');
 	// newField2.setAttribute('class','survey_options');
-	newField.setAttribute('class','ID');
+	newField2.setAttribute('class','ID');
 	newField2.setAttribute('siz',50);
-	newField2.setAttribute('placeholder','ID');
+	newField2.setAttribute('placeholder','ID (Nếu item gồm nhiều ID, ngăn cách các ID giữa các dấu phẩy)');
 	survey_options.appendChild(newField2);
+	var node = document.createElement("HR"); 
+	survey_options.appendChild(node);
 }
 
 remove_fields.onclick = function(){
@@ -30,33 +32,50 @@ remove_fields.onclick = function(){
 }
 
 function getData() {
-	var type = document.getElementsByClassName("typeOfItem").value;
+	var type = document.getElementsByClassName('typeOfItem');
 	var id = document.getElementsByClassName("ID");
 	var title = document.getElementById("title").value;
 	var desc = document.getElementById("desc").value;	
 
 	var item = [];
 
+	console.log(type);
+
 	
 	alert(type);
 	
-    // alert("Xác nhận tải xuống?");
-    // var trealet = "";
-    //     trealetFooter = "\t\t]\n"
-    //                     + "\t}\n"
-    //                     + "}";
-    //     trealetHeader = '{\n'
-    //                     + "\t\"trealet\":{\n"
-    //                     + "\t\t\"exec\":\"streamline\",\n" 
-    //                     + "\t\t\"title\":" + '\"' + title + "\",\n"
-    //                     + "\t\t\"author\":\"Nhom06\",\n"
-    //                     + "\t\t\"desc\":" + '\"' + desc + "\",\n";
-    //                     + "\t\t\"items\": [\n"
-    //     // itemPhoto = "\t\t\t\{\"title\":\"Một số ảnh\", \"value\":" + photo + "},\n";
-    //     // itemGif = "\t\t\t\{\"title\":\"GIF\", \"value\": " + gif + "},\n";
-    //     // itemVideo =  "\t\t\t\{\"title\":\"Video giới thiệu\", \"value\": " + video + "},\n";
-    //     // item3d = "\t\t\t\{\"title\":\"Tham quan trực tuyến\", \"value\": " + _3d + "}\n";
-    // download('newFile.trealet', trealet);
+    alert("Xác nhận tải xuống?");
+    var trealet = "";
+        trealetFooter = "\t\t]\n"
+                        + "\t}\n"
+                        + "}";
+        trealetHeader = '{\n'
+                        + "\t\"trealet\":{\n"
+                        + "\t\t\"exec\":\"streamline\",\n" 
+                        + "\t\t\"title\":" + '\"' + title + "\",\n"
+                        + "\t\t\"author\":\"Nhom06\",\n"
+                        + "\t\t\"desc\":" + '\"' + desc + "\",\n"
+                        + "\t\t\"items\": [\n";
+		trealetItem = "";
+		for (let i = 0; i < type.length; i++) {
+			if (i == type.length - 1)
+				if (id[i].value.includes(','))
+					trealetItem += `\t\t\t\{\"title\":\"${type[i]}\", \"value\":[${id[i]}]\}\n`;
+				else
+				trealetItem += `\t\t\t\{\"title\":\"${type[i]}\", \"value\":${id[i]}\}\n`;
+			else
+				if (id[i].value.includes(','))
+					trealetItem += `\t\t\t\{\"title\":\"${type[i]}\", \"value\":[${id[i]}]\},\n`;
+				else
+				trealetItem += `\t\t\t\{\"title\":\"${type[i].value}\", \"value\":${id[i].value}\},\n`;
+		}
+		trealet = trealetHeader + trealetItem + trealetFooter;
+		console.log(trealet);
+        // itemPhoto = "\t\t\t\{\"title\":\"Một số ảnh\", \"value\":" + photo + "},\n";
+        // itemGif = "\t\t\t\{\"title\":\"GIF\", \"value\": " + gif + "},\n";
+        // itemVideo =  "\t\t\t\{\"title\":\"Video giới thiệu\", \"value\": " + video + "},\n";
+        // item3d = "\t\t\t\{\"title\":\"Tham quan trực tuyến\", \"value\": " + _3d + "}\n";
+    download('newFile.trealet', trealet);
 }
 
 function download(filename, text) {
