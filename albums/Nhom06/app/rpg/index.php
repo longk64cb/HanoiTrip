93568cfebe2f414c85cf774b12d2ff80a325fb7c
NDM2108 <63856585+NDM2108@users.noreply.github.com>
2021-12-06 21:48:26 +0700

    loading page 2

--- albums/Nhom06/app/rpg/index.php
+++ albums/Nhom06/app/rpg/index.php
@@ -13,7 +13,7 @@
         <link rel="shortcut icon" href="logo.png" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link rel="stylesheet" href="style.css?ver=25">
-        <link rel="stylesheet" href="showOff.css?ver=37">
+        <link rel="stylesheet" href="showOff.css?ver=43">
         <style>
             .map {
                 background-image: url(<?php echo $d["map"]?>);
